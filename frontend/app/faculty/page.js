5b76867c99d3b0aac78f7a716faf588795c86b83
aditibaskaran3 <aditibaskaran.cse2024@citchennai.net>
2026-06-18 14:05:09 +0530

    Color modification

--- frontend/app/faculty/page.js
+++ frontend/app/faculty/page.js
@@ -827,7 +827,7 @@ export default function Home() {
                                         <div className="flex flex-col items-end gap-2">
 
                                             {currentAssessment?.results_published ? (
-                                                <div className="bg-green-100 text-green-700 font-semibold px-1 py-2 rounded-xl border border-green-200">
+                                                <div className="bg-green-50 hover:bg-green-200 text-green-700 border border-green-400 text-sm px-4 py-2 rounded-lg font-semibold transition">
                                                     Results Published
                                                 </div>
                                             ) : (
@@ -869,7 +869,7 @@ export default function Home() {
 
                                             <button
                                                 onClick={exportCSV}
-                                                className="bg-blue-100 hover:bg-blue-200 text-blue-700 text-sm px-4 py-2 rounded-lg font-medium transition"
+                                                className="bg-blue-50 hover:bg-blue-100 text-blue-700 border border-blue-400 text-sm px-4 py-2 rounded-lg font-bold transition"
                                             >
                                                 Export CSV
                                             </button>
